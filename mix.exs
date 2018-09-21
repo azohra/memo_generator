62176aff207843dd7b695bf8f4d8ecbc02bb971c
Matthew Nielsen <matthew.nielsen3@loblaw.ca>
2018-09-21 14:19:06 -0400

    update version for new release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MemoGenerator.MixProject do
   def project do
     [
       app: :memo_generator,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
